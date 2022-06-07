5a702a5f982648f47525f4cc120dea839485368d
haner <haner.du@qq.com>
2022-06-07 17:16:35 +0800

    添加：ElTree所需静态变量

--- resources/frontend/packages/ac-shared/constant.ts
+++ resources/frontend/packages/ac-shared/constant.ts
@@ -219,3 +219,21 @@ export const DOCUMENT_TYPES = {
     OTHER: 2,
     MARKDOWN: 3,
 }
+
+export const EVENT_CODE = {
+    tab: 'Tab',
+    enter: 'Enter',
+    space: 'Space',
+    left: 'ArrowLeft', // 37
+    up: 'ArrowUp', // 38
+    right: 'ArrowRight', // 39
+    down: 'ArrowDown', // 40
+    esc: 'Escape',
+    delete: 'Delete',
+    backspace: 'Backspace',
+    numpadEnter: 'NumpadEnter',
+    pageUp: 'PageUp',
+    pageDown: 'PageDown',
+    home: 'Home',
+    end: 'End',
+}
