6d3f8cf3636ae32238a76836e882c728098bf725
haner <haner.du@qq.com>
2022-06-15 14:38:55 +0800

    构建：项目打包

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport"
       content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title>ApiCat</title>
-    <script type="module" crossorigin src="/assets/index.edf0554f.js"></script>
+    <script type="module" crossorigin src="/assets/index.0b92b67b.js"></script>
     <link rel="modulepreload" href="/assets/vendor.1b70e788.js">
     <link rel="modulepreload" href="/assets/element-plus.33434d3d.js">
     <link rel="stylesheet" href="/assets/element-plus.07fbf481.css">
