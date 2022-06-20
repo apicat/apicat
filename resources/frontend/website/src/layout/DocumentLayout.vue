40e6dc9645d26aedd5ac3d41f4d0253de150ac1d
haner <haner.du@qq.com>
2022-06-20 15:49:27 +0800

    重构：编辑界面布局调整

--- resources/frontend/website/src/layout/DocumentLayout.vue
+++ resources/frontend/website/src/layout/DocumentLayout.vue
@@ -1,11 +1,12 @@
 <template>
-    <DocumentVerticalHeader />
-
     <main class="ac-doc-layout">
         <div class="ac-doc-layout--left">
+            <ProjectInfoHeader />
             <DirectoryTree ref="directoryTree" />
         </div>
         <div class="ac-doc-layout--right">
+            <DocumentOperateHeader />
+            <div class="ac-doc-layout__gap"></div>
             <router-view />
         </div>
     </main>
@@ -14,7 +15,8 @@
     <DocumentImportModal ref="documentImportModal" />
 </template>
 <script setup lang="ts">
-    import DocumentVerticalHeader from '../views/document/components/DocumentVerticalHeader.vue'
+    import ProjectInfoHeader from '../views/document/components/ProjectInfoHeader.vue'
+    import DocumentOperateHeader from '../views/document/components/DocumentOperateHeader.vue'
     import DirectoryTree from '../views/document/components/DirectoryTree.vue'
     import DocumentImportModal from '../views/document/components/DocumentImportModal.vue'
     import { ref, provide } from 'vue'
---
+++ resources/frontend/website/src/views/document/components/DocumentOperateHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="ac-header-operate">
+        <div class="ac-header-operate__main">
+            <p :class="titleClass">13123</p>
+        </div>
+
+        <div class="ac-header-operate__btns">
+            <el-button type="primary">保存</el-button>
+            <i class="iconfont iconshare2"></i>
+            <i class="iconfont iconIconPopoverUpload"></i>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    import { ref, computed } from 'vue'
+    import emitter, { IS_SHOW_DOCUMENT_TITLE } from '@/common/emitter'
+
+    const isShowTitle = ref(false)
+    const titleClass = computed(() => {
+        return [
+            'ac-header-operate__title animate__animated animate__faster',
+            {
+                hidden: !isShowTitle.value,
+                animate__slideInDown: isShowTitle.value,
+                animate__slideOutUp: !isShowTitle.value,
+            },
+        ]
+    })
+
+    emitter.on(IS_SHOW_DOCUMENT_TITLE, (isShow: any) => {
+        isShowTitle.value = isShow
+    })
+</script>
