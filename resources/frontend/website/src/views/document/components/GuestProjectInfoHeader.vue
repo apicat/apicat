973814888b1ca5b26995efa1a55d9b7c10476d4b
haner <haner.du@qq.com>
2022-06-24 10:54:47 +0800

    文档：项目名称后私有icon位置展示问题

--- resources/frontend/website/src/views/document/components/GuestProjectInfoHeader.vue
+++ resources/frontend/website/src/views/document/components/GuestProjectInfoHeader.vue
@@ -4,10 +4,12 @@
             <a href="/"> <img :src="project.icon" :alt="project.name" /></a>
         </div>
 
-        <div class="ac-project-info__title" :title="project.name">{{ project.name }}</div>
-        <el-tooltip effect="dark" content="私有项目" placement="bottom" v-if="isPrivate">
-            <el-icon class="ac-project-info__icon"><Lock /></el-icon>
-        </el-tooltip>
+        <div class="ac-project-info__title" :title="project.name">
+            {{ project.name }}
+            <el-tooltip effect="dark" content="私有项目" placement="bottom" v-if="isPrivate">
+                <el-icon class="ac-project-info__icon"><Lock /></el-icon>
+            </el-tooltip>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
