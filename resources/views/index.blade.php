deb6957bdf455fd8bed56e6af069431f1dfd012d
haner <haner.du@qq.com>
2022-06-30 18:12:07 +0800

    打包：前端资源构建

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,11 @@
     <meta name="viewport"
       content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title>ApiCat</title>
-    <script type="module" crossorigin src="/assets/index.a22b0d3c.js"></script>
-    <link rel="modulepreload" href="/assets/vendor.b27f3889.js">
-    <link rel="modulepreload" href="/assets/element-plus.85f938a9.js">
+    <script type="module" crossorigin src="/assets/index.f06094a1.js"></script>
+    <link rel="modulepreload" href="/assets/vendor.cec8f34c.js">
+    <link rel="modulepreload" href="/assets/element-plus.274d913c.js">
     <link rel="stylesheet" href="/assets/element-plus.7d952092.css">
-    <link rel="stylesheet" href="/assets/index.a9be210f.css">
+    <link rel="stylesheet" href="/assets/index.9fd19b00.css">
   </head>
 
   <body>
