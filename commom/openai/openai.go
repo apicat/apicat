239bbf11050fb22e1d6dde8dd1d909673fbf2ba0
Gunn <helloworld.gunn@gmail.com>
2023-04-22 16:10:24 +0800

    修改调用openai的接口

--- commom/openai/openai.go
+++ commom/openai/openai.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	openAI "github.com/sashabaranov/go-openai"
 )
@@ -21,16 +22,32 @@ var createApiPromptZh = "\"\"\"\n为%s设计一个 http api，并以 OpenAPI3.0.
 var createSchemaPromptEn = "\"\"\"\nDesign a json schema format for the %v and return.\n\"\"\"\n"
 var createSchemaPromptZh = "\"\"\"\n为%s设计一个 json schema 格式，并返回内容。 \n\"\"\"\n"
 
+var createApiBySchemaEn = "\"\"\"\nPlease generate a \"%s\" API based on the json schema content I provided below, and return it in the data format of openapi3.0.0.\n%s\n\"\"\"\n"
+var createApiBySchemaZh = "\"\"\"\n请根据我下面提供的json schema内容生成一个 \"%s\" 的api，并以openapi3.0.0的数据格式返回。\n%s\n\"\"\"\n"
+
+var listApiBySchemaEn = "\"\"\"\nBelow I will provide a json schema content named %s, which APIs can be generated according to this schema? Please directly provide the API names that can be generated in the form of an array, Inappropriate parameters can be ignored.\n%s\n\"\"\"\n"
+var listApiBySchemaZh = "\"\"\"\n下面我会提供一个名为%s的json schema内容，根据这个schema可以生成哪些API？ 请直接以数组形式给出可生成的API名称。\n%s\n\"\"\"\n"
+
 func NewOpenAI(token, language string) *OpenAI {
 	return &OpenAI{
 		token:     token,
-		language:  language,
+		language:  strings.ToLower(language),
 		maxTokens: 500,
 	}
 }
 
 func (o *OpenAI) CreateApi(apiName string) (string, error) {
-	prompt := o.generatePrompt(apiName, "createApi")
+	prompt := o.generatePrompt("createApi", apiName)
+	err := o.createCompletion(prompt)
+	if err != nil {
+		return "", err
+	}
+
+	return o.CompletionResponse.Choices[0].Text, nil
+}
+
+func (o *OpenAI) CreateApiBySchema(apiName, schemaContent string) (string, error) {
+	prompt := o.generatePrompt("createApiBySchema", apiName, schemaContent)
 	err := o.createCompletion(prompt)
 	if err != nil {
 		return "", err
@@ -40,7 +57,17 @@ func (o *OpenAI) CreateApi(apiName string) (string, error) {
 }
 
 func (o *OpenAI) CreateSchema(schemaName string) (string, error) {
-	prompt := o.generatePrompt(schemaName, "createSchema")
+	prompt := o.generatePrompt("createSchema", schemaName)
+	err := o.createCompletion(prompt)
+	if err != nil {
+		return "", err
+	}
+
+	return o.CompletionResponse.Choices[0].Text, nil
+}
+
+func (o *OpenAI) ListApiBySchema(schemaName, schemaContent string) (string, error) {
+	prompt := o.generatePrompt("listApiBySchema", schemaName, schemaContent)
 	err := o.createCompletion(prompt)
 	if err != nil {
 		return "", err
@@ -80,23 +107,28 @@ func (o *OpenAI) createCompletion(prompt string) error {
 	return nil
 }
 
-func (o *OpenAI) generatePrompt(text, action string) string {
+func (o *OpenAI) generatePrompt(action string, text ...string) string {
 	switch action {
 	case "createApi":
 		if o.language == "zh" {
-			return fmt.Sprintf(createApiPromptZh, text)
+			return fmt.Sprintf(createApiPromptZh, text[0])
 		}
-		return fmt.Sprintf(createApiPromptEn, text)
+		return fmt.Sprintf(createApiPromptEn, text[0])
 	case "createSchema":
 		if o.language == "zh" {
-			return fmt.Sprintf(createSchemaPromptZh, text)
+			return fmt.Sprintf(createSchemaPromptZh, text[0])
 		}
-		return fmt.Sprintf(createSchemaPromptEn, text)
+		return fmt.Sprintf(createSchemaPromptEn, text[0])
 	case "createApiBySchema":
 		if o.language == "zh" {
-			return fmt.Sprintf(createApiPromptZh, text)
+			return fmt.Sprintf(createApiBySchemaZh, text[0], text[1])
 		}
-		return fmt.Sprintf(createApiPromptEn, text)
+		return fmt.Sprintf(createApiBySchemaEn, text[0], text[1])
+	case "listApiBySchema":
+		if o.language == "zh" {
+			return fmt.Sprintf(listApiBySchemaZh, text[0], text[1])
+		}
+		return fmt.Sprintf(listApiBySchemaEn, text[0], text[1])
 	default:
 		return ""
 	}
--- commom/openai/openai_test.go
+++ commom/openai/openai_test.go
@@ -37,3 +37,49 @@ func TestCreateSchema(t *testing.T) {
 	}
 	fmt.Println(res)
 }
+
+func TestListApiBySchema(t *testing.T) {
+	token, err := os.ReadFile("./testdata/openai_token.txt")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	schema, err := os.ReadFile("./testdata/customer_schema.json")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	o := NewOpenAI(string(token), "en")
+	o.SetMaxTokens(3000)
+	res, err := o.ListApiBySchema("Customer", string(schema))
+	if err != nil || res == "" {
+		t.Log(err)
+		t.Fail()
+	}
+	fmt.Println(res)
+}
+
+func TestCreateApiBySchema(t *testing.T) {
+	token, err := os.ReadFile("./testdata/openai_token.txt")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	schema, err := os.ReadFile("./testdata/customer_schema.json")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	o := NewOpenAI(string(token), "en")
+	o.SetMaxTokens(3000)
+	res, err := o.CreateApiBySchema("CreateCustomer", string(schema))
+	if err != nil || res == "" {
+		t.Log(err)
+		t.Fail()
+	}
+	fmt.Println(res)
+}
