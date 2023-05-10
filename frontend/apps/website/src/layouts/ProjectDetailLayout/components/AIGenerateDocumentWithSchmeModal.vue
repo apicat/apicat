40f86d5e4a8ee7e6b4e7209e7b564eb30ec9f2c5
Han <haner.du@qq.com>
2023-05-10 16:04:36 +0800

    refactor:ai generate api request param

--- frontend/apps/website/src/layouts/ProjectDetailLayout/components/AIGenerateDocumentWithSchmeModal.vue
+++ frontend/apps/website/src/layouts/ProjectDetailLayout/components/AIGenerateDocumentWithSchmeModal.vue
@@ -109,8 +109,9 @@ const handleCreate = async (selectedRows: Array<any>) => {
     item.abortController = new AbortController()
 
     try {
-      const { description: title, id: schema_id, method, path } = item
-      const data: any = await createCollectionByAI({ project_id, parent_id, schema_id, title, method, path }, { signal: item.abortController.signal })
+      const { description: title, method, path } = item
+      const data: any = await createCollectionByAI({ project_id, parent_id, schema_id: item.schema.id, title, method, path }, { signal: item.abortController.signal })
+
       item.isSuccess = true
       item.isFinish = true
       item.isLoading = false
