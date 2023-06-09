73fbbf64d30a96e3501431599ece782f28ea5933
Han <haner.du@qq.com>
2023-06-09 17:10:16 +0800

    refactor:control project member permission logic

--- frontend/apps/website/src/router/filter/index.ts
+++ frontend/apps/website/src/router/filter/index.ts
@@ -1,8 +1,10 @@
 import type { Router } from 'vue-router'
-import { setupGetProjectInfoFilter } from './getProjectInfo'
+import { setupGetProjectInfoFilter } from './projectInfo.filter'
+import { setupMemberPermissionFilter } from './memberPermission.filter'
 import { setupAuthFilter } from './auth.filter'
 
 export const setupRouterFilter = (router: Router) => {
   setupAuthFilter(router)
   setupGetProjectInfoFilter(router)
+  setupMemberPermissionFilter(router)
 }
