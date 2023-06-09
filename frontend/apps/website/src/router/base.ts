241ea4f71eff80b706005d3aa6011e3fd7a9fcad
Han <haner.du@qq.com>
2023-06-09 12:28:22 +0800

    refactor:add permission page & router

--- frontend/apps/website/src/router/base.ts
+++ frontend/apps/website/src/router/base.ts
@@ -1,11 +1,17 @@
-export const LOGIN_PATH = '/login'
-export const REGISTER_PATH = '/register'
+import { LOGIN_PATH, NO_PERMISSION_PATH, REGISTER_PATH } from './constant'
 
 export const notFoundRoute = {
   path: '/:path(.*)*',
   name: 'error',
   meta: { ignoreAuth: true },
   component: () => import('@/views/errors/NotFound.vue'),
+}
+
+export const noPermissionRoute = {
+  path: NO_PERMISSION_PATH,
+  name: 'no.permission',
+  meta: { ignoreAuth: true },
+  component: () => import('@/views/errors/NoPermission.vue'),
 }
 
 export const loginRoute = {
