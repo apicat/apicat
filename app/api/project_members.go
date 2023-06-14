45120f7959018b4098c93a09bc2e9d8d5183f1e9
facai <wangfacai.mail@gmail>
2023-06-14 15:45:07 +0800

    fix: email desensitization of project member list

--- app/api/project_members.go
+++ app/api/project_members.go
@@ -3,6 +3,7 @@ package api
 import (
 	"math"
 	"net/http"
+	"strings"
 
 	"github.com/apicat/apicat/common/translator"
 	"github.com/apicat/apicat/enum"
@@ -94,13 +95,15 @@ func ProjectMembersList(ctx *gin.Context) {
 
 	membersList := []any{}
 	for _, v := range members {
+		email := userIDToNameMap[v.UserID].Email
+		parts := strings.Split(email, "@")
 		membersList = append(membersList, map[string]any{
 			"id":         v.ID,
 			"user_id":    v.UserID,
 			"username":   userIDToNameMap[v.UserID].Username,
 			"authority":  v.Authority,
 			"is_enabled": userIDToNameMap[v.UserID].IsEnabled,
-			"email":      userIDToNameMap[v.UserID].Email,
+			"email":      parts[0][0:1] + "***" + parts[0][len(parts[0])-1:] + "@" + parts[len(parts)-1],
 			"created_at": v.CreatedAt.Format("2006-01-02 15:04:05"),
 		})
 	}
