1b2ff9d9458df3bbe481b68e7fa71e602dce6af0
afocus <wangyinliang@hotmail.com>
2023-06-19 23:46:38 +0800

    chore(export):Markdown APIs support folders

--- common/spec/plugin/export/md.go
+++ common/spec/plugin/export/md.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/apicat/apicat/common/spec"
@@ -32,10 +33,23 @@ func Markdown(in *spec.Spec) ([]byte, error) {
 		}
 	}
 
+	sort.Slice(list, func(i, j int) bool {
+		v, x := list[i], list[j]
+		a, b := paths[v[0]][v[1]], paths[x[0]][x[1]]
+		return a.Dir < b.Dir
+	})
+
 	buf.WriteString("\n## Table of APIs\n")
+	var lastdir string
 	for k, v := range list {
 		item := paths[v[0]][v[1]]
-		fmt.Fprintf(&buf, "%d. [%s](#api-%d)\n", k+1, item.Title, k+1)
+		if item.Dir != lastdir {
+			if item.Dir != "" {
+				fmt.Fprintf(&buf, "- %s\n", item.Dir)
+			}
+			lastdir = item.Dir
+		}
+		fmt.Fprintf(&buf, "  - **%s** [%d.%s](#api-%d)\n", strings.ToUpper(v[1]), k+1, item.Title, k+1)
 	}
 
 	buf.WriteString("\n\n")
@@ -55,41 +69,52 @@ func rednerHttpPart(buf *bytes.Buffer, i int, path, method string, part spec.HTT
 	fmt.Fprintf(buf, "## <span id=\"api-%d\">%d. %s</span>\n", i, i, part.Title)
 	fmt.Fprintf(buf, "### Path\n [%s](%s)\n", path, path)
 	fmt.Fprintf(buf, "### Method\n %s\n", strings.ToUpper(method))
-	fmt.Fprintf(buf, "### Parameters\n")
-	renderTableHeader(buf, paramsHeaderCols)
-	for k, v := range part.Parameters.Map() {
-		for _, item := range v {
-			buf.WriteString("|")
-			renderString(buf, item.Name)
-			buf.WriteString("|**")
-			buf.WriteString(k)
-			buf.WriteString("**|`")
-			buf.WriteString(item.Schema.Type.Value()[0])
-			buf.WriteString("` | ")
-			if item.Required {
-				buf.WriteString("*")
+	params := part.Parameters.Map()
+	if len(params) > 0 {
+		var renderHeader bool
+		for k, v := range params {
+			for _, item := range v {
+				if !renderHeader {
+					fmt.Fprintf(buf, "### Parameters\n")
+					renderTableHeader(buf, paramsHeaderCols)
+					renderHeader = true
+				}
+
+				buf.WriteString("|")
+				renderString(buf, item.Name)
+				buf.WriteString("|**")
+				buf.WriteString(k)
+				buf.WriteString("**|`")
+				buf.WriteString(item.Schema.Type.Value()[0])
+				buf.WriteString("` | ")
+				if item.Required {
+					buf.WriteString("*")
+				}
+				buf.WriteString(" | ")
+				renderString(buf, item.Schema.Description)
+				buf.WriteByte('\n')
 			}
-			buf.WriteString(" | ")
-			renderString(buf, item.Schema.Description)
-			buf.WriteByte('\n')
 		}
 	}
-	fmt.Fprintf(buf, "### Request Body\n")
-	for k, v := range part.Content {
-		fmt.Fprintf(buf, "ContentType `%s`\n", k)
-		renderTableHeader(buf, jsonschemaHeaderCols)
-		if v.Schema == nil {
-			continue
-		}
-		renderSchema(buf, "`root`", 0, true, v.Schema)
-		if strings.Contains(k, "json") {
-			b, _ := json.Marshal(v.Schema)
-			if rx, err := datagen.JSONSchemaGen(b, &datagen.GenOption{DatagenKey: "x-apicat-mock"}); err == nil {
-				buf.WriteString("\n\nExample\n\n")
-				buf.WriteString("\n```json\n")
-				mockexample, _ := json.MarshalIndent(rx, "", "  ")
-				buf.Write(mockexample)
-				buf.WriteString("\n```\n\n")
+
+	if len(part.Content) > 0 {
+		fmt.Fprintf(buf, "### Request Body\n")
+		for k, v := range part.Content {
+			fmt.Fprintf(buf, "ContentType `%s`\n", k)
+			renderTableHeader(buf, jsonschemaHeaderCols)
+			if v.Schema == nil {
+				continue
+			}
+			renderSchema(buf, "`root`", 0, true, v.Schema)
+			if strings.Contains(k, "json") {
+				b, _ := json.Marshal(v.Schema)
+				if rx, err := datagen.JSONSchemaGen(b, &datagen.GenOption{DatagenKey: "x-apicat-mock"}); err == nil {
+					buf.WriteString("\n\nExample\n\n")
+					buf.WriteString("\n```json\n")
+					mockexample, _ := json.MarshalIndent(rx, "", "  ")
+					buf.Write(mockexample)
+					buf.WriteString("\n```\n\n")
+				}
 			}
 		}
 	}
