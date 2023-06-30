e06c12fb5f4fba6d522f06d2152bc1aee3489d30
Han <haner.du@qq.com>
2023-06-30 09:40:18 +0800

    feat:add back top comp

---
+++ frontend/apps/website/src/components/AcBacktop.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-backtop v-bind="$attrs" class="ac-backtop">
+    <el-icon><ac-icon-ep-arrow-up-bold /></el-icon>
+  </el-backtop>
+</template>
+
+<style lang="scss" scoped>
+@use '@/styles/mixins/mixins' as *;
+
+@include b(backtop) {
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 2px;
+  -webkit-box-shadow: 0 1px 3px rgb(0 0 0 / 20%);
+  box-shadow: 0 1px 3px rgb(0 0 0 / 20%);
+  -webkit-transition: all 0.2s ease-in-out;
+  -o-transition: all 0.2s ease-in-out;
+  transition: all 0.2s ease-in-out;
+
+  i {
+    color: #fff;
+    font-size: 20px;
+  }
+}
+</style>
