2ea16cca439e05e5fccf09312bb12a2406305f48
facai <wangfacai.mail@gmail>
2023-07-20 14:45:24 +0800

    fix: collection history list

--- app/api/doc_histories.go
+++ app/api/doc_histories.go
@@ -10,6 +10,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CollectionHistoryListData struct {
+	ID       uint                        `json:"id"`
+	Title    string                      `json:"title"`
+	Type     string                      `json:"type"`
+	SubNodes []CollectionHistoryListData `json:"sub_nodes,omitempty"`
+}
+
 func CollectionHistoryList(ctx *gin.Context) {
 	uriData := CollectionDataGetData{}
 	if err := translator.ValiadteTransErr(ctx, ctx.ShouldBindUri(&uriData)); err != nil {
@@ -42,7 +49,7 @@ func CollectionHistoryList(ctx *gin.Context) {
 		return
 	}
 
-	r1 := map[string][]any{}
+	r1 := map[string][]CollectionHistoryListData{}
 	for _, v := range histories {
 		month := v.CreatedAt.Format("2006-01")
 
@@ -52,19 +59,20 @@ func CollectionHistoryList(ctx *gin.Context) {
 			username = userDict[v.CreatedBy].Username
 		}
 
-		r1[month] = append(r1[month], map[string]any{
-			"id":    v.ID,
-			"title": fmt.Sprintf("%s(%s)", date, username),
+		r1[month] = append(r1[month], CollectionHistoryListData{
+			ID:    v.ID,
+			Title: fmt.Sprintf("%s(%s)", date, username),
+			Type:  v.Type,
 		})
 	}
 
-	r2 := []any{}
+	r2 := []CollectionHistoryListData{}
 	for k, v := range r1 {
-		r2 = append(r2, map[string]any{
-			"id":        0,
-			"title":     fmt.Sprintf("%s月", strings.Replace(k, "-", "年", -1)),
-			"type":      0,
-			"sub_nodes": v,
+		r2 = append(r2, CollectionHistoryListData{
+			ID:       0,
+			Title:    fmt.Sprintf("%s月", strings.Replace(k, "-", "年", -1)),
+			Type:     "category",
+			SubNodes: v,
 		})
 	}
 
