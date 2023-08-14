be447e9043986f76873a4dd0b3a7ca78dc40567b
Han <haner.du@qq.com>
2023-08-14 16:45:51 +0800

    refactor:router named

--- frontend/apps/website/src/router/definition.schema.ts
+++ frontend/apps/website/src/router/definition.schema.ts
@@ -4,21 +4,18 @@ import { RouteRecordRaw } from 'vue-router'
 import { MemberAuthorityInProject } from '@/typings/member'
 
 export const SCHEMA_EDIT_NAME = 'definition.schema.edit'
-export const SCHEMA_DETAIL_NAME = 'definition.schema.detail'
-
 export const SCHEMA_DETAIL_PATH = PROJECT_DETAIL_PATH + '/schema/:schema_id?'
+
+export const SCHEMA_DETAIL_NAME = 'definition.schema.detail'
 export const SCHEMA_EDIT_PATH = PROJECT_DETAIL_PATH + '/schema/:schema_id/edit'
 
 export const getSchemaDetailPath = (project_id: number | string, schema_id: number | string) => compile(SCHEMA_DETAIL_PATH)({ project_id, schema_id })
 export const getSchemaEditPath = (project_id: number | string, schema_id: number | string) => compile(SCHEMA_EDIT_PATH)({ project_id, schema_id })
 
-const SchemaEditPage = () => import('@/views/document/SchemaEditPage.vue')
-const SchemaDetailPage = () => import('@/views/document/SchemaDetailPage.vue')
-
 export const schemaDetailRoute: RouteRecordRaw = {
   name: SCHEMA_DETAIL_NAME,
   path: SCHEMA_DETAIL_PATH,
-  component: SchemaDetailPage,
+  component: () => import('@/views/document/SchemaDetailPage.vue'),
   meta: {
     ignoreAuth: true,
   },
@@ -27,7 +24,7 @@ export const schemaDetailRoute: RouteRecordRaw = {
 export const schemaEditRoute: RouteRecordRaw = {
   name: SCHEMA_EDIT_NAME,
   path: SCHEMA_EDIT_PATH,
-  component: SchemaEditPage,
+  component: () => import('@/views/document/SchemaEditPage.vue'),
   meta: {
     editableRoles: [MemberAuthorityInProject.MANAGER, MemberAuthorityInProject.WRITE],
   },
