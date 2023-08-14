374b3916c75090f8879aa32dba0f2196c99f8e5e
afocus <wangyinliang@hotmail.com>
2023-08-14 23:26:18 +0800

    diff:add remove

--- backend/common/spec/diff/diff.go
+++ backend/common/spec/diff/diff.go
@@ -9,7 +9,7 @@ import (
 var (
 	diffNew    = "+"
 	diffRemove = "-"
-	diffUpdate = "*"
+	diffUpdate = "!"
 	// diffType        = "type"
 	// diffName        = "name"
 	// diffDescription = "description"
@@ -23,7 +23,7 @@ var (
 // spec.Collections 里面只能有一个接口
 // 返回对比后的两个接口 其中只有最新的那个 也就是target里边会通过x-apicat-diff标记是否有差异
 // 差异并不包含排序
-func Diff(source, target *spec.Spec) (*spec.CollectItem, *spec.CollectItem) {
+func Diff(source, target *spec.Spec, del bool) (*spec.CollectItem, *spec.CollectItem) {
 	if len(source.Collections) != 1 || len(target.Collections) != 1 {
 		panic("source,target Collections length error")
 	}
@@ -32,8 +32,8 @@ func Diff(source, target *spec.Spec) (*spec.CollectItem, *spec.CollectItem) {
 	if au.Method != bu.Method || au.Path != bu.Method {
 		bu.XDiff = &diffUpdate
 	}
-	equalRequest(&a.HTTPRequestNode, &b.HTTPRequestNode)
-	equalResponse(a.Responses, b.Responses)
+	equalRequest(&a.HTTPRequestNode, &b.HTTPRequestNode, del)
+	equalResponse(a.Responses, b.Responses, del)
 	return a.ToCollectItem(*au), b.ToCollectItem(*bu)
 }
 
@@ -49,44 +49,66 @@ func getMapOne(d map[string]map[string]spec.HTTPPart) (*spec.HTTPPart, *spec.HTT
 	return nil, nil
 }
 
-func equalParam(a spec.HTTPParameters, b *spec.HTTPParameters) {
+func equalParam(a spec.HTTPParameters, b *spec.HTTPParameters, del bool) {
 	a1 := a.Map()
 	b1 := b.Map()
 	for k, v := range b1 {
 		x := a1[k]
-		equalSchemas(x, v)
+		equalSchemas(x, v, del)
 	}
 }
 
-func equalSchemas(a, b spec.Schemas) {
-	if a == nil || b == nil {
-		return
+func equalSchemas(a, b spec.Schemas, del bool) {
+	if del {
+		for i, v := range a {
+			if s := b.Lookup(v.Name); s == nil {
+				newv := *v
+				newv.XDiff = &diffRemove
+				if len(b) < i {
+					b = slices.Insert(b, i, &newv)
+				} else {
+					b = append(b, v)
+				}
+			}
+		}
 	}
 	for _, v := range b {
+		if v.XDiff == &diffRemove {
+			continue
+		}
 		if s := a.Lookup(v.Name); s == nil {
 			v.XDiff = &diffNew
 		} else {
-			equalJsonSchema(v.Schema, s.Schema)
+			equalJsonSchema(v.Schema, s.Schema, del)
 		}
 	}
 }
 
-func equalContent(a, b spec.HTTPBody) {
+func equalContent(a, b spec.HTTPBody, del bool) {
 	for k, v := range b {
 		if x, ok := a[k]; !ok {
 			v.XDiff = &diffNew
 		} else {
-			equalSchema(x, v)
+			equalSchema(x, v, del)
+		}
+	}
+	if del {
+		for k, v := range a {
+			if _, ok := b[k]; !ok {
+				newv := *v
+				newv.XDiff = &diffRemove
+				b[k] = &newv
+			}
 		}
 	}
 }
 
-func equalRequest(a, b *spec.HTTPRequestNode) {
-	equalParam(a.Parameters, &b.Parameters)
-	equalContent(a.Content, b.Content)
+func equalRequest(a, b *spec.HTTPRequestNode, del bool) {
+	equalParam(a.Parameters, &b.Parameters, del)
+	equalContent(a.Content, b.Content, del)
 }
 
-func equalResponse(a, b spec.HTTPResponses) {
+func equalResponse(a, b spec.HTTPResponses, del bool) {
 	aa := a.Map()
 	for _, v := range b {
 		if x, ok := aa[v.Code]; ok {
@@ -94,28 +116,38 @@ func equalResponse(a, b spec.HTTPResponses) {
 			case x.Name != v.Name || x.Description != v.Description:
 				v.XDiff = &diffUpdate
 			default:
-				equalSchemas(x.Header, v.Header)
-				equalContent(x.Content, v.Content)
+				equalSchemas(x.Header, v.Header, del)
+				equalContent(x.Content, v.Content, del)
 			}
 		} else {
 			v.XDiff = &diffNew
 		}
 	}
+	if del {
+		bb := b.Map()
+		for _, v := range a {
+			if _, ok := bb[v.Code]; !ok {
+				newv := v
+				newv.XDiff = &diffRemove
+				b = append(b, newv)
+			}
+		}
+	}
 }
 
-func equalSchema(a, b *spec.Schema) {
+func equalSchema(a, b *spec.Schema, del bool) {
 	switch {
 	case a.Name != b.Name:
 	case a.Description != b.Description:
 	case a.Required != b.Required:
 	default:
-		equalJsonSchema(a.Schema, b.Schema)
+		equalJsonSchema(a.Schema, b.Schema, del)
 		return
 	}
 	b.XDiff = &diffUpdate
 }
 
-func equalJsonSchema(a, b *jsonschema.Schema) {
+func equalJsonSchema(a, b *jsonschema.Schema, del bool) {
 	if !slices.Equal(a.Type.Value(), b.Type.Value()) {
 		b.XDiff = &diffUpdate
 		return
@@ -135,12 +167,21 @@ func equalJsonSchema(a, b *jsonschema.Schema) {
 				if slices.Contains(b.Required, k) != slices.Contains(a.Required, k) {
 					v.XDiff = &diffUpdate
 				} else {
-					equalJsonSchema(x, v)
+					equalJsonSchema(x, v, del)
+				}
+			}
+		}
+		if del {
+			for k, v := range a.Properties {
+				if _, ok := b.Properties[k]; !ok {
+					newv := *v
+					newv.XDiff = &diffRemove
+					b.Properties[k] = &newv
 				}
 			}
 		}
 	case "array":
-		equalJsonSchema(a.Items.Value(), b.Items.Value())
+		equalJsonSchema(a.Items.Value(), b.Items.Value(), del)
 	}
 	equalJsonSchemaNormal(a, b)
 }
