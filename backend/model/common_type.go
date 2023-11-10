46bd87ebf7f16028d93100fad49f61ffc15797be
dufei <wd345901051@163.com>
2023-11-10 14:10:11 +0800

    Move common_type from models to model

---
+++ backend/model/common_type.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"strconv"
+	"strings"
+)
+
+type IdToNameMap map[uint]string
+type VirtualIDToIDMap map[int64]uint
+
+type RefContentVirtualIDToId struct {
+	DefinitionSchemas    VirtualIDToIDMap
+	DefinitionResponses  VirtualIDToIDMap
+	DefinitionParameters VirtualIDToIDMap
+	GolbalParameters     VirtualIDToIDMap
+}
+
+func ReplaceVirtualIDToID(content string, nameIDMap VirtualIDToIDMap, prefix string) string {
+	for virtualID, id := range nameIDMap {
+		oldStr := prefix + strconv.Itoa(int(virtualID))
+		newStr := prefix + strconv.Itoa(int(id))
+
+		content = strings.Replace(content, oldStr, newStr, -1)
+	}
+	return content
+}
