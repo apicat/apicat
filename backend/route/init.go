7da969f662655b11d3b36c7a4da75255a6bfc8cc
dufei <wd345901051@163.com>
2023-11-13 10:34:24 +0800

    change mock and config route model.

--- backend/route/init.go
+++ backend/route/init.go
@@ -2,9 +2,13 @@ package route
 
 import (
 	"github.com/apicat/apicat/backend/config"
+	"github.com/apicat/apicat/backend/module/translator"
+	"github.com/apicat/apicat/backend/route/middleware/db"
+	"github.com/apicat/apicat/backend/route/middleware/log"
 	"github.com/apicat/apicat/frontend"
 	"github.com/gin-gonic/gin"
 	"html/template"
+	"net/http"
 )
 
 func Init() {
@@ -13,9 +17,33 @@ func Init() {
 	r := gin.New()
 	r.ContextWithFallback = true
 
-	t, _ := template.ParseFS(frontend.FrontDist, "dist/templates/*.tmpl")
+	r.Use(
+		log.RequestIDLog("/assets/", "/static/"),
+		db.CheckDBConnStatus("/assets/", "/static/"),
+		translator.UseValidatori18n(),
+		gin.Recovery(),
+	)
+
+	t, _ := template.ParseFS(frontend.Dist, "dist/templates/*.tmpl")
 	r.SetHTMLTemplate(t)
 
-	InitApiRouter(r)
+	// 前端单页路由以及静态资源
+	frontendHandle(r)
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.FileFromFS("dist/", http.FS(frontend.Dist))
+	})
+
+	registerMock(r)
+	registerGetConfig(r)
+
+	apiRouter := r.Group("/api")
+
+	registerSetConfig(apiRouter)
+
+	InitApiRouter(apiRouter)
+
+	registerNoRoute(r)
+
 	r.Run(config.GetSysConfig().App.Host.Value + ":" + config.GetSysConfig().App.Port.Value)
 }
--- backend/route/routes.go
+++ backend/route/routes.go
@@ -1,7 +1,6 @@
 package route
 
 import (
-	"github.com/apicat/apicat/backend/common/translator"
 	"github.com/apicat/apicat/backend/route/api/ai"
 	"github.com/apicat/apicat/backend/route/api/collection"
 	"github.com/apicat/apicat/backend/route/api/config"
@@ -15,271 +14,255 @@ import (
 	"github.com/apicat/apicat/backend/route/api/trash"
 	"github.com/apicat/apicat/backend/route/api/user"
 	"github.com/apicat/apicat/backend/route/middleware/check"
-	"github.com/apicat/apicat/backend/route/middleware/db"
 	"github.com/apicat/apicat/backend/route/middleware/jwt"
-	"github.com/apicat/apicat/backend/route/middleware/log"
 	"github.com/apicat/apicat/frontend"
-	"io/fs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func InitApiRouter(r *gin.Engine) {
-
-	r.Use(
-		log.RequestIDLog("/assets/", "/static/"),
-		db.CheckDBConnStatus("/assets/", "/static/"),
-		translator.UseValidatori18n(),
-		gin.Recovery(),
-	)
-
-	assets, err := fs.Sub(frontend.FrontDist, "dist/assets")
-	if err != nil {
-		panic(err)
-	}
-	r.StaticFS("/assets", http.FS(assets))
-
-	static, err := fs.Sub(frontend.FrontDist, "dist/static")
-	if err != nil {
-		panic(err)
-	}
-	r.StaticFS("/static", http.FS(static))
-
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.FileFromFS("dist/", http.FS(frontend.FrontDist))
-	})
+func registerMock(r *gin.Engine) {
 
 	mocksrv := mock.NewMockServer()
 
 	r.Any("/mock/:id/*path", mocksrv.Handler)
 
+}
+
+func registerGetConfig(r *gin.Engine) {
 	configs := r.Group("/config")
 	{
 		configs.GET("/db", config.GetDBConfig)
 	}
+}
 
-	apiRouter := r.Group("/api")
+func registerSetConfig(g *gin.RouterGroup) {
+	configs := g.Group("/config")
 	{
-		configs := apiRouter.Group("/config")
+		configs.PUT("/db", config.SetDBConfig)
+	}
+}
+
+// TODO 分类，分组
+func InitApiRouter(g *gin.RouterGroup) {
+	mocksrv := mock.NewMockServer()
+
+	// 未登录状态下可访问的API
+	notLogin := g.Group("")
+	{
+		account := notLogin.Group("/account")
 		{
-			configs.PUT("/db", config.SetDBConfig)
+			account.POST("/login/email", user.EmailLogin)
+			account.POST("/register/email", user.EmailRegister)
 		}
 
-		// 未登录状态下可访问的API
-		notLogin := apiRouter.Group("")
+		projects := notLogin.Group("/projects")
+		projects.Use(check.CheckProject())
 		{
-			account := notLogin.Group("/account")
-			{
-				account.POST("/login/email", user.EmailLogin)
-				account.POST("/register/email", user.EmailRegister)
-			}
-
-			projects := notLogin.Group("/projects")
-			projects.Use(check.CheckProject())
-			{
-				projects.GET("/:project-id/data", project.ProjectDataGet)
-				projects.GET("/:project-id/share/status", check.CheckMemberHalfLogin(), project.ProjectShareStatus)
-				projects.POST("/:project-id/share/check", project.ProjectShareSecretkeyCheck)
-			}
-
-			collections := notLogin.Group("/projects/:project-id/collections")
-			collections.Use(check.CheckProject())
-			{
-				collections.GET("/:collection-id/data", check.CheckCollection(), collection.CollectionDataGet)
-				collections.POST("/:collection-id/share/check", check.CheckCollection(), doc.DocShareCheck)
-			}
-
-			collectionShare := notLogin.Group("/collections")
-			{
-				collectionShare.GET("/:public_collection_id/share/status", doc.DocShareStatus)
-			}
+			projects.GET("/:project-id/data", project.ProjectDataGet)
+			projects.GET("/:project-id/share/status", check.CheckMemberHalfLogin(), project.ProjectShareStatus)
+			projects.POST("/:project-id/share/check", project.ProjectShareSecretkeyCheck)
 		}
 
-		// 半登录状态下可访问的API。半登录：登录或不登录时都可访问，但响应的参数不同
-		halfLogin := apiRouter.Group("")
-		halfLogin.Use(check.CheckProject(), check.CheckMemberHalfLogin(), check.CheckProjectMemberHalfLogin(), mocksrv.ClearCache())
+		collections := notLogin.Group("/projects/:project-id/collections")
+		collections.Use(check.CheckProject())
 		{
-			projects := halfLogin.Group("/projects/:project-id")
-			{
-				projects.GET("", project.ProjectsGet)
-			}
-
-			servers := halfLogin.Group("/projects/:project-id/servers")
-			{
-				servers.GET("", server.UrlList)
-			}
-
-			definitionSchemas := halfLogin.Group("/projects/:project-id/definition/schemas")
-			{
-				definitionSchemas.GET("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasGet)
-				definitionSchemas.GET("", definition.DefinitionSchemasList)
-			}
-
-			collections := halfLogin.Group("/projects/:project-id/collections")
-			{
-				collections.GET("", collection.CollectionsList)
-				collections.GET("/:collection-id", check.CheckCollection(), collection.CollectionsGet)
-			}
-
-			globalParameters := halfLogin.Group("/projects/:project-id/global/parameters")
-			{
-				globalParameters.GET("", global.GlobalParametersList)
-			}
-
-			definitionResponses := halfLogin.Group("/projects/:project-id/definition/responses")
-			{
-				definitionResponses.GET("", definition.DefinitionResponsesList)
-				definitionResponses.GET("/:response-id", definition.DefinitionResponsesDetail)
-			}
+			collections.GET("/:collection-id/data", check.CheckCollection(), collection.CollectionDataGet)
+			collections.POST("/:collection-id/share/check", check.CheckCollection(), doc.DocShareCheck)
 		}
 
-		// 仅登录状态下可访问的API。仅登录：仅登录了apicat便可访问，一般为项目外的操作
-		onlyLogin := apiRouter.Group("")
-		onlyLogin.Use(check.CheckMember())
+		collectionShare := notLogin.Group("/collections")
 		{
-			users := onlyLogin.Group("/user")
-			{
-				users.GET("/self", user.GetUserInfo)
-				users.PUT("/self", user.SetUserInfo)
-				users.PUT("/self/password", user.ChangePassword)
-			}
-
-			members := onlyLogin.Group("/members")
-			{
-				members.GET("", project.GetMembers)
-				members.POST("/", project.AddMember)
-				members.PUT("/:user-id", project.SetMember)
-				members.DELETE("/:user-id", project.DeleteMember)
-			}
-
-			projects := onlyLogin.Group("/projects")
-			{
-				projects.GET("", project.ProjectsList)
-				projects.POST("", project.ProjectsCreate)
-			}
-
-			iterations := onlyLogin.Group("/iterations")
-			{
-				iterations.GET("", iteration.IterationsList)
-				iterations.GET("/:iteration-id", iteration.IterationsDetails)
-				iterations.POST("", iteration.IterationsCreate)
-				iterations.PUT("/:iteration-id", iteration.IterationsUpdate)
-				iterations.DELETE("/:iteration-id", iteration.IterationsDelete)
-			}
-
-			projectGroup := onlyLogin.Group("/project_group")
-			{
-				projectGroup.GET("", project.ProjectGroupList)
-				projectGroup.POST("", project.ProjectGroupCreate)
-				projectGroup.PUT("/:group_id/rename", project.ProjectGroupRename)
-				projectGroup.DELETE("/:group_id", project.ProjectGroupDelete)
-				projectGroup.PUT("/order", project.ProjectGroupOrder)
-			}
-		}
-
-		// 项目内部操作
-		projects := apiRouter.Group("/projects/:project-id")
-		projects.Use(jwt.JWTAuthMiddleware(), check.CheckProject(), check.CheckProjectMember(), mocksrv.ClearCache())
-		{
-			projects := projects.Group("")
-			{
-				projects.PUT("", project.ProjectsUpdate)
-				projects.DELETE("", project.ProjectsDelete)
-				projects.DELETE("/exit", project.ProjectExit)
-				projects.PUT("/transfer", project.ProjectTransfer)
-				projects.GET("/share", project.ProjectShareDetails)
-				projects.PUT("/share/switch", project.ProjectSharingSwitch)
-				projects.PUT("/share/reset", project.ProjectShareReset)
-				projects.POST("/follow", project.ProjectFollow)
-				projects.DELETE("/follow", project.ProjectUnFollow)
-				projects.PUT("/change_group", project.ProjectChangeGroup)
-			}
-
-			definitionSchemas := projects.Group("/definition/schemas")
-			{
-				definitionSchemas.POST("", definition.DefinitionSchemasCreate)
-				definitionSchemas.PUT("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasUpdate)
-				definitionSchemas.DELETE("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasDelete)
-				definitionSchemas.POST("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasCopy)
-				definitionSchemas.PUT("/movement", definition.DefinitionSchemasMove)
-			}
-
-			servers := projects.Group("/servers")
-			{
-				servers.PUT("", server.UrlSettings)
-			}
-
-			globalParameters := projects.Group("/global/parameters")
-			{
-				globalParameters.POST("", global.GlobalParametersCreate)
-				globalParameters.PUT("/:parameter-id", global.GlobalParametersUpdate)
-				globalParameters.DELETE("/:parameter-id", global.GlobalParametersDelete)
-			}
-
-			definitionResponses := projects.Group("/definition/responses")
-			{
-				definitionResponses.POST("", definition.DefinitionResponsesCreate)
-				definitionResponses.PUT("/:response-id", definition.DefinitionResponsesUpdate)
-				definitionResponses.DELETE("/:response-id", definition.DefinitionResponsesDelete)
-			}
-
-			collections := projects.Group("/collections")
-			{
-				collections.POST("", collection.CollectionsCreate)
-				collections.PUT("/:collection-id", check.CheckCollection(), collection.CollectionsUpdate)
-				collections.POST("/:collection-id", check.CheckCollection(), collection.CollectionsCopy)
-				collections.PUT("/movement", collection.CollectionsMovement)
-				collections.DELETE("/:collection-id", check.CheckCollection(), collection.CollectionsDelete)
-				collections.GET("/:collection-id/share", check.CheckCollection(), doc.DocShareDetails)
-				collections.PUT("/:collection-id/share/switch", check.CheckCollection(), doc.DocShareSwitch)
-				collections.PUT("/:collection-id/share/reset", check.CheckCollection(), doc.DocShareReset)
-			}
-
-			trashs := projects.Group("/trashs")
-			{
-				trashs.GET("", trash.TrashsList)
-				trashs.PUT("", trash.TrashsRecover)
-			}
-
-			ais := projects.Group("/ai")
-			{
-				ais.GET("/collections/name", ai.AICreateApiNames)
-				ais.POST("/collections", ai.AICreateCollection)
-				ais.POST("/schemas", ai.AICreateSchema)
-			}
-
-			projectMember := projects.Group("/members")
-			{
-				projectMember.GET("", project.ProjectMembersList)
-				projectMember.POST("", project.ProjectMembersCreate)
-				projectMember.PUT("/authority/:user-id", project.ProjectMembersAuthUpdate)
-				projectMember.DELETE("/:user-id", project.ProjectMembersDelete)
-				projectMember.GET("/without", project.ProjectMembersWithout)
-			}
-
-			collectionHistories := projects.Group("/collections/:collection-id/histories")
-			collectionHistories.Use(check.CheckCollection())
-			{
-				collectionHistories.GET("", doc.CollectionHistoryList)
-				collectionHistories.GET("/:history-id", doc.CollectionHistoryDetails)
-				collectionHistories.GET("/diff", doc.CollectionHistoryDiff)
-				collectionHistories.PUT("/:history-id/restore", doc.CollectionHistoryRestore)
-
-			}
-
-			definitionSchemaHistories := projects.Group("/definition/schemas/:schemas-id/histories")
-			definitionSchemaHistories.Use(check.CheckDefinitionSchema())
-			{
-				definitionSchemaHistories.GET("", definition.DefinitionSchemaHistoryList)
-				definitionSchemaHistories.GET("/:history-id", definition.DefinitionSchemaHistoryDetails)
-				definitionSchemaHistories.GET("/diff", definition.DefinitionSchemaHistoryDiff)
-				definitionSchemaHistories.PUT("/:history-id/restore", definition.DefinitionSchemaHistoryRestore)
-			}
+			collectionShare.GET("/:public_collection_id/share/status", doc.DocShareStatus)
 		}
 	}
 
+	// 半登录状态下可访问的API。半登录：登录或不登录时都可访问，但响应的参数不同
+	halfLogin := g.Group("")
+	halfLogin.Use(check.CheckProject(), check.CheckMemberHalfLogin(), check.CheckProjectMemberHalfLogin(), mocksrv.ClearCache())
+	{
+		projects := halfLogin.Group("/projects/:project-id")
+		{
+			projects.GET("", project.ProjectsGet)
+		}
+
+		servers := halfLogin.Group("/projects/:project-id/servers")
+		{
+			servers.GET("", server.UrlList)
+		}
+
+		definitionSchemas := halfLogin.Group("/projects/:project-id/definition/schemas")
+		{
+			definitionSchemas.GET("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasGet)
+			definitionSchemas.GET("", definition.DefinitionSchemasList)
+		}
+
+		collections := halfLogin.Group("/projects/:project-id/collections")
+		{
+			collections.GET("", collection.CollectionsList)
+			collections.GET("/:collection-id", check.CheckCollection(), collection.CollectionsGet)
+		}
+
+		globalParameters := halfLogin.Group("/projects/:project-id/global/parameters")
+		{
+			globalParameters.GET("", global.GlobalParametersList)
+		}
+
+		definitionResponses := halfLogin.Group("/projects/:project-id/definition/responses")
+		{
+			definitionResponses.GET("", definition.DefinitionResponsesList)
+			definitionResponses.GET("/:response-id", definition.DefinitionResponsesDetail)
+		}
+	}
+
+	// 仅登录状态下可访问的API。仅登录：仅登录了apicat便可访问，一般为项目外的操作
+	onlyLogin := g.Group("")
+	onlyLogin.Use(check.CheckMember())
+	{
+		users := onlyLogin.Group("/user")
+		{
+			users.GET("/self", user.GetUserInfo)
+			users.PUT("/self", user.SetUserInfo)
+			users.PUT("/self/password", user.ChangePassword)
+		}
+
+		members := onlyLogin.Group("/members")
+		{
+			members.GET("", project.GetMembers)
+			members.POST("/", project.AddMember)
+			members.PUT("/:user-id", project.SetMember)
+			members.DELETE("/:user-id", project.DeleteMember)
+		}
+
+		projects := onlyLogin.Group("/projects")
+		{
+			projects.GET("", project.ProjectsList)
+			projects.POST("", project.ProjectsCreate)
+		}
+
+		iterations := onlyLogin.Group("/iterations")
+		{
+			iterations.GET("", iteration.IterationsList)
+			iterations.GET("/:iteration-id", iteration.IterationsDetails)
+			iterations.POST("", iteration.IterationsCreate)
+			iterations.PUT("/:iteration-id", iteration.IterationsUpdate)
+			iterations.DELETE("/:iteration-id", iteration.IterationsDelete)
+		}
+
+		projectGroup := onlyLogin.Group("/project_group")
+		{
+			projectGroup.GET("", project.ProjectGroupList)
+			projectGroup.POST("", project.ProjectGroupCreate)
+			projectGroup.PUT("/:group_id/rename", project.ProjectGroupRename)
+			projectGroup.DELETE("/:group_id", project.ProjectGroupDelete)
+			projectGroup.PUT("/order", project.ProjectGroupOrder)
+		}
+	}
+
+	// 项目内部操作
+	projects := g.Group("/projects/:project-id")
+	projects.Use(jwt.JWTAuthMiddleware(), check.CheckProject(), check.CheckProjectMember(), mocksrv.ClearCache())
+	{
+		projects := projects.Group("")
+		{
+			projects.PUT("", project.ProjectsUpdate)
+			projects.DELETE("", project.ProjectsDelete)
+			projects.DELETE("/exit", project.ProjectExit)
+			projects.PUT("/transfer", project.ProjectTransfer)
+			projects.GET("/share", project.ProjectShareDetails)
+			projects.PUT("/share/switch", project.ProjectSharingSwitch)
+			projects.PUT("/share/reset", project.ProjectShareReset)
+			projects.POST("/follow", project.ProjectFollow)
+			projects.DELETE("/follow", project.ProjectUnFollow)
+			projects.PUT("/change_group", project.ProjectChangeGroup)
+		}
+
+		definitionSchemas := projects.Group("/definition/schemas")
+		{
+			definitionSchemas.POST("", definition.DefinitionSchemasCreate)
+			definitionSchemas.PUT("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasUpdate)
+			definitionSchemas.DELETE("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasDelete)
+			definitionSchemas.POST("/:schemas-id", check.CheckDefinitionSchema(), definition.DefinitionSchemasCopy)
+			definitionSchemas.PUT("/movement", definition.DefinitionSchemasMove)
+		}
+
+		servers := projects.Group("/servers")
+		{
+			servers.PUT("", server.UrlSettings)
+		}
+
+		globalParameters := projects.Group("/global/parameters")
+		{
+			globalParameters.POST("", global.GlobalParametersCreate)
+			globalParameters.PUT("/:parameter-id", global.GlobalParametersUpdate)
+			globalParameters.DELETE("/:parameter-id", global.GlobalParametersDelete)
+		}
+
+		definitionResponses := projects.Group("/definition/responses")
+		{
+			definitionResponses.POST("", definition.DefinitionResponsesCreate)
+			definitionResponses.PUT("/:response-id", definition.DefinitionResponsesUpdate)
+			definitionResponses.DELETE("/:response-id", definition.DefinitionResponsesDelete)
+		}
+
+		collections := projects.Group("/collections")
+		{
+			collections.POST("", collection.CollectionsCreate)
+			collections.PUT("/:collection-id", check.CheckCollection(), collection.CollectionsUpdate)
+			collections.POST("/:collection-id", check.CheckCollection(), collection.CollectionsCopy)
+			collections.PUT("/movement", collection.CollectionsMovement)
+			collections.DELETE("/:collection-id", check.CheckCollection(), collection.CollectionsDelete)
+			collections.GET("/:collection-id/share", check.CheckCollection(), doc.DocShareDetails)
+			collections.PUT("/:collection-id/share/switch", check.CheckCollection(), doc.DocShareSwitch)
+			collections.PUT("/:collection-id/share/reset", check.CheckCollection(), doc.DocShareReset)
+		}
+
+		trashs := projects.Group("/trashs")
+		{
+			trashs.GET("", trash.TrashsList)
+			trashs.PUT("", trash.TrashsRecover)
+		}
+
+		ais := projects.Group("/ai")
+		{
+			ais.GET("/collections/name", ai.AICreateApiNames)
+			ais.POST("/collections", ai.AICreateCollection)
+			ais.POST("/schemas", ai.AICreateSchema)
+		}
+
+		projectMember := projects.Group("/members")
+		{
+			projectMember.GET("", project.ProjectMembersList)
+			projectMember.POST("", project.ProjectMembersCreate)
+			projectMember.PUT("/authority/:user-id", project.ProjectMembersAuthUpdate)
+			projectMember.DELETE("/:user-id", project.ProjectMembersDelete)
+			projectMember.GET("/without", project.ProjectMembersWithout)
+		}
+
+		collectionHistories := projects.Group("/collections/:collection-id/histories")
+		collectionHistories.Use(check.CheckCollection())
+		{
+			collectionHistories.GET("", doc.CollectionHistoryList)
+			collectionHistories.GET("/:history-id", doc.CollectionHistoryDetails)
+			collectionHistories.GET("/diff", doc.CollectionHistoryDiff)
+			collectionHistories.PUT("/:history-id/restore", doc.CollectionHistoryRestore)
+
+		}
+
+		definitionSchemaHistories := projects.Group("/definition/schemas/:schemas-id/histories")
+		definitionSchemaHistories.Use(check.CheckDefinitionSchema())
+		{
+			definitionSchemaHistories.GET("", definition.DefinitionSchemaHistoryList)
+			definitionSchemaHistories.GET("/:history-id", definition.DefinitionSchemaHistoryDetails)
+			definitionSchemaHistories.GET("/diff", definition.DefinitionSchemaHistoryDiff)
+			definitionSchemaHistories.PUT("/:history-id/restore", definition.DefinitionSchemaHistoryRestore)
+		}
+	}
+
+}
+
+func registerNoRoute(r *gin.Engine) {
 	r.NoRoute(func(ctx *gin.Context) {
-		ctx.FileFromFS("dist/", http.FS(frontend.FrontDist))
+		ctx.FileFromFS("dist/", http.FS(frontend.Dist))
 	})
 }
