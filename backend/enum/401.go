535b16cc3310642f73e29b474d6b5c5ccdd82c0c
dufei <wd345901051@163.com>
2023-11-13 16:11:40 +0800

    move content from enum 403,404 to enum 401 and remove 403 and 404 file.

--- backend/enum/401.go
+++ backend/enum/401.go
@@ -5,4 +5,18 @@ const (
 	InvalidOrIncorrectLoginToken = 101
 	// 访问令牌（分享密钥）无效或不正确
 	InvalidOrIncorrectAccessToken = 201
+
+	// 成员权限不足
+	MemberInsufficientPermissionsCode = 101
+	// 项目成员权限不足
+	ProjectMemberInsufficientPermissionsCode = 201
+	// 目标项目成员权限不足
+	TargetProjectMemberInsufficientPermissionsCode = 202
+	// 跳转403页面
+	Redirect403Page = 301
+
+	// 跳转404页面
+	Redirect404Page = 101
+	// 显示404错误信息
+	Display404ErrorMessage = 201
 )
