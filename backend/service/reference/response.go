067f4a029d3a962700b49c188fa84953351f224b
facai <wangfacai.mail@gmail>
2024-05-09 18:06:45 +0800

    modify definition response reference

--- backend/service/reference/response.go
+++ backend/service/reference/response.go
@@ -2,119 +2,96 @@ package reference
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/apicat/apicat/v2/backend/model/collection"
 	"github.com/apicat/apicat/v2/backend/model/definition"
-	referencerelationship "github.com/apicat/apicat/v2/backend/model/reference_relationship"
+	referencerelation "github.com/apicat/apicat/v2/backend/model/reference_relation"
 	arrutil "github.com/apicat/apicat/v2/backend/utils/array"
 )
 
 // UpdateResponseRef 更新公共响应引用关系
-func UpdateResponseRef(ctx context.Context, r *definition.DefinitionResponse) error {
-	rr := &referencerelationship.ResponseReference{ResponseID: r.ID}
-	lastRefs, err := rr.GetResponseRefs(ctx)
+func UpdateResponseRef(ctx context.Context, r *definition.DefinitionResponse, oldScheamIDs []uint) error {
+	newSchemaIDs := ParseRefSchemas(r.Content)
+
+	if err := updateRefResponseToScheam(ctx, r.ID, oldScheamIDs, newSchemaIDs); err != nil {
+		return err
+	}
+
+	// 预留response还会引用其他公共组建的情况
+	return nil
+}
+
+// updateRefResponseToScheam 更新公共响应引用公共模型的引用关系
+func updateRefResponseToScheam(ctx context.Context, rID uint, oldScheamIDs, newSchemaIDs []uint) error {
+	oldRefs, err := referencerelation.GetRefSchemaResponses(ctx, oldScheamIDs...)
 	if err != nil {
 		return err
 	}
 
-	lastRefsMap := make(map[uint]*referencerelationship.ResponseReference, 0)
-	for _, v := range lastRefs {
-		lastRefsMap[v.RefSchemaID] = v
+	oldRefsMap := make(map[uint]*referencerelation.RefSchemaResponses, 0)
+	for _, v := range oldRefs {
+		oldRefsMap[v.RefSchemaID] = v
 	}
 
-	latestRefs := ParseRefSchemas(r.Content)
-
 	wantPop := make([]uint, 0)
-	wantPush := make([]*referencerelationship.ResponseReference, 0)
+	wantPush := make([]*referencerelation.RefSchemaResponses, 0)
 
 	// 删除老引用关系中存在但当前引用中不存在的引用
-	for key, value := range lastRefsMap {
-		if !arrutil.InArray[uint](key, latestRefs) {
+	for key, value := range oldRefsMap {
+		if !arrutil.InArray(key, newSchemaIDs) {
 			wantPop = append(wantPop, value.ID)
 		}
 	}
 
 	// 添加老引用关系中不存在但当前引用中存在的引用
-	for _, v := range latestRefs {
-		if _, ok := lastRefsMap[v]; !ok {
-			wantPush = append(wantPush, &referencerelationship.ResponseReference{
-				ResponseID:  r.ID,
+	for _, v := range newSchemaIDs {
+		if _, ok := oldRefsMap[v]; !ok {
+			wantPush = append(wantPush, &referencerelation.RefSchemaResponses{
+				ResponseID:  rID,
 				RefSchemaID: v,
 			})
 		}
 	}
 
-	if err := referencerelationship.BatchCreateResponseReference(ctx, wantPush); err != nil {
+	if err := referencerelation.BatchCreateRefSchemaResponses(ctx, wantPush); err != nil {
 		return err
 	}
-	return referencerelationship.BatchDeleteResponseReference(ctx, wantPop...)
+	return referencerelation.BatchDelRefSchemaResponses(ctx, wantPop...)
 }
 
 // DerefResponse 公共响应解引用
 func DerefResponse(ctx context.Context, r *definition.DefinitionResponse, deref bool) error {
+	nowSchemaIDs := ParseRefSchemas(r.Content)
+
+	rc := referencerelation.RefResponseCollections{RefResponserID: r.ID}
+	cIDs, err := rc.GetCollectionIDs(ctx)
+	if err != nil {
+		return err
+	}
+
+	// 在collection中解引用response
+	if err := derefResponseFromCollections(ctx, r, cIDs, deref); err != nil {
+		return err
+	}
+
 	if deref {
-		return unpackResponseRef(ctx, r)
-	} else {
-		return clearResponseRef(ctx, r)
-	}
-}
-
-// clearResponseRef 清除公共响应
-func clearResponseRef(ctx context.Context, r *definition.DefinitionResponse) error {
-	// 清除被引用关系(collections -> self)
-	cr := &referencerelationship.CollectionReference{RefID: r.ID, RefType: referencerelationship.ReferenceResponse}
-	collectionIDs, err := cr.GetCollectionIDsByRef(ctx)
-	if err != nil {
-		return err
-	}
-
-	if err := derefResponseFromCollections(ctx, r, collectionIDs, false); err != nil {
-		return err
-	}
-
-	// 清除引用关系(self -> scheams)
-	return deleteResponseReference(ctx, r)
-}
-
-// unpackResponseRef 展开公共响应
-func unpackResponseRef(ctx context.Context, r *definition.DefinitionResponse) error {
-	// 清除被引用关系(collections -> self)
-	cr := &referencerelationship.CollectionReference{RefID: r.ID, RefType: referencerelationship.ReferenceResponse}
-	collectionIDs, err := cr.GetCollectionIDsByRef(ctx)
-	if err != nil {
-		return err
-	}
-
-	if err := derefResponseFromCollections(ctx, r, collectionIDs, true); err != nil {
-		return err
-	}
-
-	// 建立自身引用的(self -> schemas 中的schemas)与引用自身的(collections -> self 中的collections)之间的引用关系
-	rr := referencerelationship.ResponseReference{ResponseID: r.ID}
-	responseRefRecords, err := rr.GetResponseRefs(ctx)
-	if err != nil {
-		return err
-	}
-
-	collectionWantPush := make([]*referencerelationship.CollectionReference, 0)
-	for _, v := range responseRefRecords {
-		for _, c := range collectionIDs {
-			collectionWantPush = append(collectionWantPush, &referencerelationship.CollectionReference{
-				CollectionID: c,
-				RefID:        v.RefSchemaID,
-				RefType:      referencerelationship.ReferenceSchema,
-			})
+		// 建立引用自身的(collections -> self 中的collections)与自身引用的(self -> schemas 中的schemas)之间的引用关系
+		if err := linkResponseRefContextParentCWithChildS(ctx, cIDs, nowSchemaIDs); err != nil {
+			return err
 		}
 	}
-	if err := referencerelationship.BatchCreateCollectionReference(ctx, collectionWantPush); err != nil {
+
+	// 清除引用关系(collections -> self)
+	if err := clearRefCollectionsToResponse(ctx, r.ID); err != nil {
 		return err
 	}
 
 	// 清除引用关系(self -> scheams)
-	return deleteResponseReference(ctx, r)
+	return clearRefResponseToSchemas(ctx, r.ID, nowSchemaIDs)
 }
 
-// derefResponseFromCollections 从集合中解引用公共响应
+// derefResponseFromCollections 解开collection中引用response的地方
 func derefResponseFromCollections(ctx context.Context, r *definition.DefinitionResponse, collectionIDs []uint, deref bool) error {
 	collections, err := collection.GetCollections(ctx, r.ProjectID, collectionIDs...)
 	if err != nil {
@@ -122,25 +99,35 @@ func derefResponseFromCollections(ctx context.Context, r *definition.DefinitionR
 	}
 
 	for _, c := range collections {
-		c.DelRefResponse(ctx, r, deref)
+		if err := c.DelRefResponse(ctx, r, deref); err != nil {
+			slog.ErrorContext(ctx, "c.DelRefResponse", "err", err)
+		}
 	}
 
-	cr := &referencerelationship.CollectionReference{RefID: r.ID, RefType: referencerelationship.ReferenceResponse}
-	return cr.DelByRef(ctx)
+	return nil
 }
 
-// deleteResponseReference 删除公共响应引用关系
-func deleteResponseReference(ctx context.Context, r *definition.DefinitionResponse) error {
-	rr := referencerelationship.ResponseReference{ResponseID: r.ID}
-	refs, err := rr.GetResponseRefs(ctx)
-	if err != nil {
-		return err
+// clearRefCollectionsToResponse 清除collections引用response的引用关系
+func clearRefCollectionsToResponse(ctx context.Context, rID uint) error {
+	return referencerelation.DelRefResponseCollections(ctx, []uint{rID})
+}
+
+// clearRefResponseToSchemas 清除response引用schemas的引用关系
+func clearRefResponseToSchemas(ctx context.Context, rID uint, schemaIDs []uint) error {
+	return referencerelation.DelRefSchemaResponse(ctx, schemaIDs, rID)
+}
+
+// linkResponseRefContextParentCWithChildS 建立respnose引用父级collections与引用子集的schemas之间的引用关系
+func linkResponseRefContextParentCWithChildS(ctx context.Context, collectionIDs, schemaIDs []uint) error {
+	wantPush := make([]*referencerelation.RefSchemaCollections, 0)
+	for _, v := range schemaIDs {
+		for _, c := range collectionIDs {
+			wantPush = append(wantPush, &referencerelation.RefSchemaCollections{
+				RefSchemaID:  v,
+				CollectionID: c,
+			})
+		}
 	}
 
-	var ids []uint
-	for _, item := range refs {
-		ids = append(ids, item.ID)
-	}
-
-	return referencerelationship.BatchDeleteResponseReference(ctx, ids...)
+	return referencerelation.BatchCreateRefSchemaCollections(ctx, wantPush)
 }
