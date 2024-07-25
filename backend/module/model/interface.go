36a52bd2dcd96c6b0552358c8dbb334737e33b75
Gang <helloworld.gunn@gmail.com>
2024-07-25 16:18:03 +0800

    Feature/b embedding (#45)
    * Updates go-openai package version and Add embedding func in model module
    
    * Modify the interface in the module model

--- backend/module/model/interface.go
+++ backend/module/model/interface.go
@@ -4,6 +4,7 @@ type Provider interface {
 	ChatMessageRoleSystem() string
 	ChatMessageRoleAssistant() string
 	ChatMessageRoleUser() string
-	ChatCompletionRequest(r *ChatCompletionOption) (string, error)
-	Check(modelType string) error
+	ChatCompletionRequest(*ChatCompletionOption) (string, error)
+	CreateEmbeddings(string) ([]float32, error)
+	Check(string) error
 }
--- backend/module/model/openai.go
+++ backend/module/model/openai.go
@@ -18,16 +18,33 @@ type OpenAI struct {
 }
 
 type AzureOpenAI struct {
-	ApiKey    string
-	Endpoint  string
-	LLM       string
-	Embedding string
+	ApiKey              string
+	Endpoint            string
+	LLM                 string
+	LLMDeployName       string
+	Embedding           string
+	EmbeddingDeployName string
 }
 
 type openai struct {
 	llm       string
 	embedding string
 	client    *oai.Client
+	ctx       context.Context
+}
+
+var LLM_SUPORTS = []string{
+	oai.GPT4,
+	oai.GPT4o,
+	oai.GPT4oMini,
+	oai.GPT4Turbo,
+	oai.GPT3Dot5Turbo,
+}
+
+var EMBEDDING_SUPORTS = []string{
+	string(oai.SmallEmbedding3),
+	string(oai.LargeEmbedding3),
+	string(oai.AdaEmbeddingV2),
 }
 
 func NewOpenAI(cfg OpenAI) *openai {
@@ -38,25 +55,40 @@ func NewOpenAI(cfg OpenAI) *openai {
 		llm:       cfg.LLM,
 		embedding: cfg.Embedding,
 		client:    oai.NewClientWithConfig(clientConfig),
+		ctx:       context.Background(),
 	}
 }
 
 func NewAzureOpenAI(cfg AzureOpenAI) *openai {
 	clientConfig := oai.DefaultAzureConfig(cfg.ApiKey, cfg.Endpoint)
 	clientConfig.HTTPClient.Timeout = time.Second * 30
+	clientConfig.AzureModelMapperFunc = func(model string) string {
+		azureModelMapping := map[string]string{
+			cfg.LLM:       cfg.LLMDeployName,
+			cfg.Embedding: cfg.EmbeddingDeployName,
+		}
+		return azureModelMapping[model]
+	}
 
 	return &openai{
 		llm:       cfg.LLM,
 		embedding: cfg.Embedding,
 		client:    oai.NewClientWithConfig(clientConfig),
+		ctx:       context.Background(),
 	}
 }
 
 func (o *openai) Check(modelType string) error {
 	switch modelType {
 	case "llm":
+		if !support(modelType, o.llm) {
+			return fmt.Errorf("llm model %s not supported", o.llm)
+		}
 		return o.CheckLLM()
 	case "embedding":
+		if !support(modelType, o.embedding) {
+			return fmt.Errorf("embedding model %s not supported", o.embedding)
+		}
 		return o.CheckEmbedding()
 	default:
 		return fmt.Errorf("unknown model type: %s", modelType)
@@ -77,13 +109,24 @@ func (o *openai) CheckEmbedding() error {
 	if o.embedding == "" {
 		return errors.New("embedding name not set")
 	}
-	// TODO: check embedding
-	return nil
+	_, err := o.CreateEmbeddings("Hello")
+	return err
+}
+
+func (o *openai) CreateEmbeddings(input string) ([]float32, error) {
+	resp, err := o.client.CreateEmbeddings(o.ctx, oai.EmbeddingRequest{
+		Input: []string{input},
+		Model: oai.EmbeddingModel(o.embedding),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Data[0].Embedding, nil
 }
 
 func (o *openai) ChatCompletionRequest(r *ChatCompletionOption) (string, error) {
 	resp, err := o.client.CreateChatCompletion(
-		context.Background(),
+		o.ctx,
 		oai.ChatCompletionRequest{
 			Model:    o.llm,
 			Messages: compileMessages(r.Messages),
@@ -118,4 +161,22 @@ func compileMessages(m ChatCompletionMessages) []oai.ChatCompletionMessage {
 		}
 	}
 	return messages
+}
+
+func support(modelType string, model string) bool {
+	switch modelType {
+	case "llm":
+		for _, v := range LLM_SUPORTS {
+			if v == model {
+				return true
+			}
+		}
+	case "embedding":
+		for _, v := range EMBEDDING_SUPORTS {
+			if v == model {
+				return true
+			}
+		}
+	}
+	return false
 }
